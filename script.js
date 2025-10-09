2aff7886e88debbb7650dcbc5c218da61a57dd54
ERNESTO EMILIANO RIVERA LOPEZ <a22300214@ceti.mx>
2025-10-08 23:55:38 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// Configuración de partículas
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#00bfff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5, random: true },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00bfff",
+      opacity: 0.3,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 140, line_linked: { opacity: 0.7 } },
+      push: { particles_nb: 4 }
+    }
+  },
+  retina_detect: true
+});
